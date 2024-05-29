9682b5dd9517465c82c211636a933bd7c459fc83
juan312 <juanalbani48@gmail.com>
2024-05-29 13:23:19 -0300

    Finished Initial Project Setup, Implemented class for storing and retrieving User Settings, Implemented class for connecting to the database

---
+++ src/main/java/com/puntosim/databaseschemadefiner/model/SettingsHelper.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.puntosim.databaseschemadefiner.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author juanv
+ */
+public class SettingsHelper {
+
+    //SETTINGS IDENTIFIERS
+    public static final String SCHEMA_NAME = "db.schema.name";
+    public static final String SERVER_IP = "db.server.ip";
+    public static final String SERVER_PORT = "db.server.port";
+    public static final String USERNAME = "db.username";
+    public static final String PASSWORD = "db.password";
+    
+    private static File settingsFile = new File("./config.properties");
+    private static Properties settings = null;
+
+    public static void loadSettings() {
+        settings = new Properties();
+
+        if (!settingsFile.exists()) {
+            return;
+        }
+
+        try (FileInputStream inputStream = new FileInputStream(settingsFile)) {
+            settings.load(inputStream);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    
+    public static void saveSettings() {
+        
+        try (OutputStream output = new FileOutputStream(settingsFile)) {
+            settings.store(output, "");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+
+    public static String getSetting(String identifier) {
+        return settings.getProperty(identifier);
+    }
+    
+    public static void setSetting(String identifier, String value) {
+        settings.setProperty(identifier, value);
+    }
+
+}
